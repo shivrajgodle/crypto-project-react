32f8781a0b0aef11822fd0c349f5ffb5e00ba309
shivrajgodle <83442544+shivrajgodle@users.noreply.github.com>
2026-01-03 14:29:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router"
+import { Home } from "./pages/Home"
+import { CoinDetail } from "./pages/CoinDetail"
+
+
+function App() {
+ 
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/coin/:id" element={<CoinDetail/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
